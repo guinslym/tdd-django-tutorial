898841f0f1efce6e20259f251f9e3624aaeb8121
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-05-19 17:10:25 -0400

    creating a table and starting a new test 2:30:42sec

--- functional_tests.py
+++ functional_tests.py
@@ -30,6 +30,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # When the hits enter, the page updates, and the page lists "1. Buy
         # peacock feathers" as an item in a to-do list.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: Buy peacock feathers',
+            [row.text for row in rows]
+        )
 
         # There is still a text box inviting her to add another item.  She
         # enters "Use peacock feathers to make a fly".
