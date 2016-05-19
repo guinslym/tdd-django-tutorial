24758e0e6407b587f92e0a540c88f35e29541d54
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-05-19 14:22:31 -0400

    bumped into an error 1hr08and 28sec

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.http import HttpRequest
 
+from lists.views import home_page
 # Create your tests here.
+
+class HomePageViewTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertIn('<title>To-Do lists</title>', response.content)
